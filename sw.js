abd421388530a4625f94256909888e799c6821cf
KirbyJeff <shadownite0130@gmail.com>
2024-12-21 20:39:55 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,14 +3,23 @@ const cacheName = 'bitburner-android';
 const appShellFiles = [
   '/*/*/*/*/*/*/*',
 ];
+const cache = await caches.open(cacheName);
 
 // Installing Service Worker
-
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(contentToCache);
-  })());
+if ('serviceWorker' in navigator) {
+  window.addEventListener('`load`', () => {
+  navigator.serviceWorker.register('sw.js', { scope:'./' })
+      .then((registration) => {
+          console.log('Registration completed successfully',registration);
+          e.waitUntil((async () => {
+          console.log('[Service Worker] Caching all: app shell and content');
+          await cache.addAll(contentToCache);
+          })());
+      })
+      .catch((error) => {
+          console.log('Registration failed', error);
+      });
+})}
 
 // Fetching content using Service Worker
 self.addEventListener('fetch', (e) => {
@@ -26,7 +35,6 @@ self.addEventListener('fetch', (e) => {
     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
     if (r) return r;
     const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
     cache.put(e.request, response.clone());
     return response;
